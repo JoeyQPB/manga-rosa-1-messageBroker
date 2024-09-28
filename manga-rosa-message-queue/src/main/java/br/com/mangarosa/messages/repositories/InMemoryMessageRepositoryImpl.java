b0446854276b6218eb4d4147a368f849e4587294
JoeyQPB <joey_qpb@hotmail.com>
2024-09-28 01:10:31 -0300

    impls linkedQueue on inMemoryMessageRepositoryImpls

--- manga-rosa-message-queue/src/main/java/br/com/mangarosa/messages/repositories/InMemoryMessageRepositoryImpl.java
+++ manga-rosa-message-queue/src/main/java/br/com/mangarosa/messages/repositories/InMemoryMessageRepositoryImpl.java
@@ -1,13 +1,16 @@
 package br.com.mangarosa.messages.repositories;
 
+import br.com.mangarosa.datastructures.abstractClasses.extd.QueueNodeMessage;
+import br.com.mangarosa.datastructures.interfaces.impl.LinkedQueue;
 import br.com.mangarosa.messages.Message;
 import br.com.mangarosa.messages.interfaces.MessageRepository;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class InMemoryMessageRepositoryImpl implements MessageRepository {
 
-    private final Map<String, List<Message>> topics;
+    private final Map<String, LinkedQueue> topics;
 
     public InMemoryMessageRepositoryImpl() {
         this.topics = new HashMap<>();
@@ -16,43 +19,42 @@ public class InMemoryMessageRepositoryImpl implements MessageRepository {
     @Override
     public void append(String topic, Message message) {
         if (this.topics.get(topic) == null) {
-            this.topics.put(topic, new ArrayList<>());
+            this.topics.put(topic, new LinkedQueue());
         }
-        topics.get(topic).add(message);
+        topics.get(topic).enqueue(new QueueNodeMessage(message));
     }
 
     @Override
     public void consumeMessage(String topic, UUID messageId) {
-        List<Message> messages = topics.get(topic);
-        if (messages == null) throw new IllegalArgumentException(topic + "not found");
+        LinkedQueue messagesQueue = topics.get(topic);
+        if (messagesQueue == null) throw new IllegalArgumentException(topic + " not found");
 
-        for (Message message : messages) {
-            String uuid = message.getId();
+        for (QueueNodeMessage node : messagesQueue) {
+            String uuid = node.getValue().getId();
             if (Objects.equals(uuid, messageId.toString())) {
-                message.setConsumed(true);
+                node.getValue().setConsumed(true);
             }
         }
     }
 
     @Override
     public List<Message> getAllNotConsumedMessagesByTopic(String topic) {
-        List<Message> messages = topics.get(topic);
-        if (messages == null) throw new IllegalArgumentException(topic + "not found");
+        LinkedQueue messagesQueue = topics.get(topic);
+        if (messagesQueue == null) throw new IllegalArgumentException(topic + " not found");
+        List<Message> messageList = new ArrayList<>();
 
-        for (Message message : messages) {
-            if (message.isConsumed()) {
-                messages.remove(message);
+        for (QueueNodeMessage node : messagesQueue) {
+            if (!node.getValue().isConsumed()) {
+                messageList.add(node.getValue());
             }
         }
-
-        return messages;
+        return messageList;
     }
 
     @Override
     public List<Message> getAllConsumedMessagesByTopic(String topic) {
-        List<Message> messages = topics.get(topic);
-        if (messages == null) throw new IllegalArgumentException(topic + "not found");
-
-        return messages.stream().filter(Message::isConsumed).toList();
+        Message[] messagesQueue = topics.get(topic).toArrayValues();
+        if (messagesQueue == null) throw new IllegalArgumentException(topic + " not found");
+        return Arrays.stream(messagesQueue).filter(Message::isConsumed).collect(Collectors.toList());
     }
 }
